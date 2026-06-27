9ca2c6d350561bec60115674becf809bb1967991
Taha_Ostadsharif <Tahabigpro@gmail.com>
2026-06-27 18:38:46 +0330

    new mod upload

---
+++ mods/7949096995_20260627183846_My_cheet_mode.py
@@ -0,0 +1,16 @@
+import bs
+
+def bsGetAPIVersion():
+    return 4
+
+def bsGetGames():
+    return []
+
+def bsGetLevels():
+    return []
+
+def bsGetControls():
+    return []
+
+def bsOnPlayerJoin(player):
+    bs.screenMessage("Cheat Mod Loaded ")
